99b5fd292ba7c7b45a4883e92f06682e2d3ae1d1
Rusty <42674949+RHQ-Rusty@users.noreply.github.com>
2022-06-02 02:51:36 -0400

    Update sizer.py
    - Added Docstring
    - Reversed Returned Tuple Variable Order

--- tools/sizer.py
+++ tools/sizer.py
@@ -5,17 +5,22 @@ class SizeFailure(Exception):
     ...
 
 def calculate_file_size(file_abspath: str) -> tuple:
+    """
+    Basic Function to Size a File. (Cross-Platform)
+    - Takes: Absolute Filepath
+    - Gives: String of File Size
+    """
     size = path.getsize(file_abspath)
     magnitude = 0
     while abs(size) >= 1024:
         magnitude += 1
         size /= 1024.0
-    return ("%.2f %s" % (size, ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'][magnitude]), size)
+    return (size, "%.2f %s" % (size, ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'][magnitude]))
 
 if __name__ == '__main__':
     if argv[1]:
         try:
-            readable, raw = calculate_file_size(argv[1])
+            raw, readable = calculate_file_size(argv[1])
             print(f"File Size (Raw): {raw}\nFile Size (Readable): {readable}")
         except:
             raise SizeFailure("Failed to Size File! Please provide an absolute or relative filepath!")
